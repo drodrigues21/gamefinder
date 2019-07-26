713c6dea195f74639ffd97a325e4f1f886005e5b
drodrigues21 <ddvilar21@hotmail.com>
2019-07-26 15:19:23 +0900

    test first commit

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         rel="stylesheet">
     <link rel="stylesheet" href="styles/main.css">
     <script src="https://kit.fontawesome.com/7fdeb94f09.js"></script>
-    <title>Gamefinder</title>
+    <title>Gamefinder david</title>
 </head>
 
 <body>
