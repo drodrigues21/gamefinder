cc94922625c301f204fb81c40dd4d9ce5e217437
drodrigues21 <ddvilar21@hotmail.com>
2019-07-26 16:46:37 +0900

    third commit test

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
 </head>
 
 <body>
-    <div class="main_container">
-        <div class="header_container">
-            <div class="catch_frase_container">
+    <div class="mainContainer">
+        <div class="headerContainer">
+            <div class="catchFraseContainer">
                 <h3>For all your gaming needs.</h3>
             </div>
-            <div class="header_content">
-                <div class="logo_container"><a href="index2.html"><img src="images/10.png" alt=""></a></div>
-                <nav class="navbar_container">
+            <div class="headerContent">
+                <div class="logoContainer"><a href="index2.html"><img src="images/10.png" alt=""></a></div>
+                <nav class="navbarContainer">
                     <ul class="navbar">
                         <li><a href="index2.html">Home</a></li>
                         <li><a href="#">About Us</a></li>
@@ -31,40 +31,40 @@
                 </nav>
             </div>
         </div>
-        <div class="search_bar_container">
+        <div class="searchBarContainer">
             <h2>Search for a game and have fun!</h2>
             <form action="">
-                <div class="search_field">
-                    <input type="text" class="search_box" placeholder="What are you looking for?">
-                    <button type="submit" class=" btn search_button"><i class="fas fa-search"></i></button>
+                <div class="searchField">
+                    <input type="text" class="searchBox" placeholder="What are you looking for?">
+                    <button type="submit" class=" btn searchButton"><i class="fas fa-search"></i></button>
                 </div>
-                <button type="submit" class="btn surprise_button">Surprise me!</button>
-                <button type="submit" class="btn other_button">What?</button>
+                <button type="submit" class="btn surpriseButton">Surprise me!</button>
+                <button type="submit" class="btn otherButton">What?</button>
             </form>
         </div>
-        <div class="top_five_container">
-            <div class="top_five_header">
+        <div class="topFiveContainer">
+            <div class="topFiveHeader">
                 <h3>Top 5 games</h3>
             </div>
-            <div class="top_five_content">
-                <div class="top_five">
+            <div class="topFiveContent">
+                <div class="topFive">
                     <?php include("smallcardgame.php") ?>
                 </div>
-                <div class="top_five">
+                <div class="topFive">
                     <?php include("smallcardgame.php") ?>
                 </div>
-                <div class="top_five">
+                <div class="topFive">
                     <?php include("smallcardgame.php") ?>
                 </div>
-                <div class="top_five">
+                <div class="topFive">
                     <?php include("smallcardgame.php") ?>
                 </div>
-                <div class="top_five">
+                <div class="topFive">
                     <?php include("smallcardgame.php") ?>
                 </div>
             </div>
         </div>
-        <div class="arrows_container">
+        <div class="arrowsContainer">
             <div class="arrows">
                 <span></span>
             </div>
--- smallcardgame.php
+++ smallcardgame.php
@@ -1,6 +1,6 @@
 
 <img src="images/example.jpg" alt="">
-<div class="top_five_description">
+<div class="topFiveDescription">
     <h3>Beer Pong</h3>
     <p>College drinking game</p>
     <a href="#">More</a>
