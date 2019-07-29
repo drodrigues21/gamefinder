5ba467da5b4eb0b739785db8f997eb8bbab09b15
Sean Sernasie <Sean@scarYviruS.local>
2019-07-29 15:24:03 +0900

    db connect

---
+++ backend/dbconnect.php
@@ -0,0 +1,6 @@
+<?
+try {
+    $db = new PDO('mysql:host=localhost;dbname=gameFinder;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (Exception $e) {
+    die('Error : ' . $e->getMessage());
+}
