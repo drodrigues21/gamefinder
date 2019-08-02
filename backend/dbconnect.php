0a1df73ec1f8f424e4165e4f2992934b33f22b8d
drodrigues21 <ddvilar21@hotmail.com>
2019-08-02 13:36:28 +0900

    dbconnect update

--- backend/dbconnect.php
+++ backend/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=gameFinder;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
     die('Error : ' . $e->getMessage());
 }
