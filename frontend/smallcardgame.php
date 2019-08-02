5ab154baa414fef2f4ceb9c4275b393ca0720b62
drodrigues21 <ddvilar21@hotmail.com>
2019-08-02 13:06:22 +0900

    changed top five card

--- frontend/smallcardgame.php
+++ frontend/smallcardgame.php
@@ -1,11 +1,21 @@
 
 <div id="elt_<?=$count;?>" class="topFive">
+    <div class="icon"><?php
+        $topPosition = $count+1;
+        echo "#$topPosition";
+    ?></div>
+    <p class="title"><?= $data['name'];?></p>
+    <p class="text">Click to see more.</p>
+</div>
+<!-- 
+<div id="elt_<?=$count;?>" class="topFive">
     <img src="../images/example.jpg" alt="">
+    <div class="icon">#1</div>
     <div class="topFiveDescription">
-        <h3><?= $data['name'];?></h3>
-        <p><?= "Rating: " . $data['rating'];?></p>
+        <h3><?= $data;?></h3>
+        <p><?= $data;?></p>
         <a href="#">More</a>
     </div>
 </div>
-
+ -->
 
