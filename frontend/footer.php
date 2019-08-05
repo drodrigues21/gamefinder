59fbb4da0e40fced3e891bfa8011e8b7602efbe6
Sean Sernasie <Sean@scarYviruS.local>
2019-08-05 17:06:35 +0900

    struct of pages (menu; footer) different pages

---
+++ frontend/footer.php
@@ -0,0 +1,9 @@
+<div class="footer_container">
+    <nav class="footerNav">
+        <ul class="footerNavLinks">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact US</a></li>
+            <li><a href="https://wcoding.com/" target="_blank">Wcoding</a></li>
+        </ul>
+    </nav>
+</div>
