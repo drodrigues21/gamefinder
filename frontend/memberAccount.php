ea998928bb782a9e564f660d2e58220d7a3f68ae
apple <apple@appleui-MacBook-Pro.local>
2019-08-09 13:23:25 +0900

    memberaccount page

--- frontend/memberAccount.php
+++ frontend/memberAccount.php
@@ -1,30 +1,26 @@
 <?php
 session_start();
-include("modalMenu.php"); 
 ?>
 
 <link rel="stylesheet" href="../styles/modalMenu.css">
 <link rel="stylesheet" href="../styles/main.css">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,700,700i&display=swap"
     rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet"> 
-<link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/7fdeb94f09.js"></script>
 
 <body>
-    <div class = "mainContainer">
-        <div class ="firstSectionContainer">
-            <h1>Account Information</h1>
+    <div class = "mainDiv">
+            <?php include("modalMenu.php"); ?>
+            <h3 class ="userHeader">Account Information</h3>
 
             <ul class = "info">
                 <li> Full Name:  </li>
-                <li> Email Address:  </li>
-                <li> User Name: </li>
+                <li> Email Address: <?php echo $_SESSION["email"]?> </li>
+                <li> User Name: <?php echo $_SESSION["signinUsername"] ?></li>
                 <li> Password: </li>
                 <li> Date of Birth: </li>
                 <li> Country: </li>
             </ul>
-        </div> 
     </div> 
 
 </body>
