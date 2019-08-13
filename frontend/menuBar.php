95b1feaf30ac250d1cad50b99958a526ea7f188e
apple <apple@appleui-MacBook-Pro.local>
2019-08-13 16:52:39 +0900

    merge

--- frontend/menuBar.php
+++ frontend/menuBar.php
@@ -1,6 +1,11 @@
 <div class="headerContainer">
     <div class="catchFraseContainer">
-        <h3>For all your gaming needs.</h3>
+        <?php if(!isset($_SESSION["id"]) AND !isset($_SESSION["signinUsername"])){
+            echo '<h3>For all your gaming needs.</h3>';
+        }else if(isset($_SESSION["id"]) AND isset($_SESSION["signinUsername"])){
+            echo '<h3>Hello, '. $_SESSION['signinUsername'] . '!</h3>'; 
+        }
+        ?>
     </div>
     <div class="headerContent">
         <div class="logoContainer"><a href="index2.html"><img src="./images/10.png" alt=""></a></div>
