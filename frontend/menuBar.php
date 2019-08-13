25293829b8bf7e119da9f1e870cf87ddcc9c26de
apple <apple@appleui-MacBook-Pro.local>
2019-08-13 16:36:04 +0900

    menubarmerge

---
+++ frontend/menuBar.php
@@ -0,0 +1,11 @@
+<div class="headerContainer">
+    <div class="catchFraseContainer">
+        <h3>For all your gaming needs.</h3>
+    </div>
+    <div class="headerContent">
+        <div class="logoContainer"><a href="index2.html"><img src="./images/10.png" alt=""></a></div>
+        <nav class="navbarContainer">
+            <?php include("modalmenu.php"); ?>
+        </nav>
+    </div>
+</div>
